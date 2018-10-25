74cccf3a1e06dc6cb4e7551d7f7c9bf960dfb2e1
danflnn2000 <40254954+danflnn2000@users.noreply.github.com>
2018-10-24 22:32:40 -0400

    Update GitStart.java

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -3,7 +3,7 @@ package com.git;
 import java.util.Iterator;
 
 public class GitStart {
-
+	// this is just a simple comment
 	public static void main(String[] args) {
 		String [] mylist = {"boys", "girls", "dogs", "cats"};
 		
