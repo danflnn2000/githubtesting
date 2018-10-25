b53a943691303a9c23d8a264da887f4430a3dfb4
danielmastrovito <dnmastrovito@gmail.com>
2018-10-24 22:52:14 -0400

    just a commit

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class GitStart {
 	// this is just a simple comment
 	public static void main(String[] args) {
-		String [] mylist = {"boys", "girls", "dogs", "cats", "birds", "chipmunks", "mice", "rats"};
+		String [] mylist = {"boys", "girls", "dogs", "cats", "birds", "chipmunks", "mice", "rats", "insects"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
