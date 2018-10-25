1ca7450cbf76a8a7d02b6d102e6d7ebae2ef725c
danielmastrovito <dnmastrovito@gmail.com>
2018-10-24 22:34:59 -0400

    this is the commit where I have my first conflicts

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -3,14 +3,14 @@ package com.git;
 import java.util.Iterator;
 
 public class GitStart {
-
+	// this is just a simple comment
 	public static void main(String[] args) {
 		String [] mylist = {"boys", "girls", "dogs", "cats"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
 		}
-
+		//This is also a simple comment
 	}
 
 }
