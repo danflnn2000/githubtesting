f3507ae0a56d4b873c6b8cd84bdcb01c2d568fd7
danielmastrovito <dnmastrovito@gmail.com>
2018-10-24 22:31:22 -0400

    myfirst commit

---
+++ GitHubProject2/src/com/git/GitStart.java
@@ -0,0 +1,16 @@
+package com.git;
+
+import java.util.Iterator;
+
+public class GitStart {
+
+	public static void main(String[] args) {
+		String [] mylist = {"boys", "girls", "dogs", "cats"};
+		
+		for (int i = 0; i < mylist.length; i++) {
+			System.out.println(mylist[i]);
+		}
+
+	}
+
+}
