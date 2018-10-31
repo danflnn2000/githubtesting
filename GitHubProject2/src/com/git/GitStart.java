a25bab5ff31b2c0ad57b396cfadfa2cabc710868
danielmastrovito <dnmastrovito@gmail.com>
2018-10-30 22:58:47 -0400

    sds

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -25,9 +25,16 @@ public class GitStart {
 		
 		//This is my workspace comment 6
 		
+<<<<<<< Upstream, based on origin/Dans_New_Branch
 		//This is my workspace comment 7
+<<<<<<< Upstream, based on branch 'Dans_New_Branch' of https://github.com/danflnn2000/githubtesting.git
 		
 		//This is my workspace comment 11
+=======
+=======
+		//This is my workspace comment 10
+>>>>>>> 9e1e377 commit comment 10 again
+>>>>>>> f616f4b fadffa
 	}
 
 }
