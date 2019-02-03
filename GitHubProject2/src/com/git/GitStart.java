abfae64bc84e7f24e19d6d1b9f5bf0fcad3b5893
danflnn2000 <40254954+danflnn2000@users.noreply.github.com>
2019-02-03 17:17:49 -0500

    Update GitStart.java
    remove comments

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -10,18 +10,7 @@ public class GitStart {
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
 		}
-		//This is also a simple comment
 
-		//This is my workspace comment 2
-
-		//This is my git comment
-		//This is my git comment 2
-		
-		//This is my workspace comment 3
-		
-		//This is my workspace comment 4
-		
-		//This is my workspace comment 5
 	}
 
 }
