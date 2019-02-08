4947c4d36b4bdcc1a71fde4fa5fe2cff67804f6d
danielmastrovito <dnmastrovito@gmail.com>
2019-02-07 22:22:48 -0500

    Second commit for 2/7

--- GitHubProject2/src/com/git/GitStartSecond.java
+++ GitHubProject2/src/com/git/GitStartSecond.java
@@ -1,9 +1,16 @@
 package com.git;
 
 public class GitStartSecond{
+<<<<<<< Updated upstream
 
 	// this is my test from git Hub
 	// this is my second test from git hub
 	// This is my first comment
 	
+	int myNumber = 10;
+	
+	for (i = 0; i<10; i++) {
+
+	}
 }
+
