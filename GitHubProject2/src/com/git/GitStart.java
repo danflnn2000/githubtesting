a7249fd6e2caf3ad81d04a537117e491c5f4bd77
danielmastrovito <dnmastrovito@gmail.com>
2019-02-18 11:55:52 -0500

    test 2

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -21,6 +21,7 @@ public class GitStart {
 		//this is my fourth change
 		//this is my fifth change
 		//this is will be an untracked change
+		//this is my second attempt at an untracked file
 
 
 		
