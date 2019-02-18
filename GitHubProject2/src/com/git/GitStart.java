0b5a67ef6725b4ea56f94a5c2f11f26c682f32f1
danielmastrovito <dnmastrovito@gmail.com>
2019-02-18 12:01:22 -0500

    reinstate file

---
+++ GitHubProject2/src/com/git/GitStart.java
@@ -0,0 +1,31 @@
+package com.git;
+
+import java.util.Iterator;
+
+public class GitStart {
+	// this is just a simple comment
+	public static void main(String[] args) {
+
+		String [] mylist = {"boys", "girls", "men", "women"};
+		
+		for (int i = 0; i < mylist.length; i++) {
+			System.out.println(mylist[i]);
+		}
+		
+		//this is my first change
+		//this is my first remote change
+		//this is my second change
+		//this is my second remote change
+		//this is my third change
+		//this is my third remote change
+		//this is my fourth change
+		//this is my fifth change
+		//this is will be an untracked change
+		//this is my second attempt at an untracked file
+		//this is my third attempt
+
+
+		
+	}
+
+}
