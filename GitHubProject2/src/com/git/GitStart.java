57fc4ac5855d07275173c7331f0050818c33a69b
danflnn2000 <40254954+danflnn2000@users.noreply.github.com>
2022-02-12 08:37:33 -0500

    Adding squirrels

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class GitStart {
 	// this is just a simple comment
 	public static void main(String[] args) {
-		String [] mylist = {"boys", "girls", "dogs"};
+		String [] mylist = {"boys", "girls", "dogs", "squirrels"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
