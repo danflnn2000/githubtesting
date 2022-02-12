778accc59d49120d20b1ed45e321045fc4814581
danielmastrovito <dnmastrovito@gmail.com>
2022-02-12 13:54:04 -0500

    adding cups

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class GitStart {
 	// this is just a simple comment
 	public static void main(String[] args) {
-		String [] mylist = {"boys", "girls", "dogs", "badgers"};
+		String [] mylist = {"boys", "girls", "dogs", "cups"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
