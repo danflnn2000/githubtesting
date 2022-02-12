b54e898ce110f8ffb4b44ed6768f496336fbabe4
danflnn2000 <40254954+danflnn2000@users.noreply.github.com>
2022-02-12 14:08:56 -0500

    adding ham
    change bacon to ham

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class GitStart {
 	// this is just a simple comment
 	public static void main(String[] args) {
-		String [] mylist = {"boys", "girls", "dogs", "bacon"};
+		String [] mylist = {"boys", "girls", "dogs", "ham"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
