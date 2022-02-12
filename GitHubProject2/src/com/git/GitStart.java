cc5ab9eb1c714d8c007ac4565ca203c403d80b63
danielmastrovito <dnmastrovito@gmail.com>
2022-02-12 08:35:17 -0500

    remove rats from array

--- GitHubProject2/src/com/git/GitStart.java
+++ GitHubProject2/src/com/git/GitStart.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 public class GitStart {
 	// this is just a simple comment
 	public static void main(String[] args) {
-		String [] mylist = {"boys", "girls", "dogs","rats"};
+		String [] mylist = {"boys", "girls", "dogs"};
 		
 		for (int i = 0; i < mylist.length; i++) {
 			System.out.println(mylist[i]);
